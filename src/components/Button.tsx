73f4660866d9f67308f627d060992e53a5153dc4
Bruno Freitas <bruno.barrt@gmail.com>
2025-07-09 11:54:39 -0300

    Ajuste css botão azul

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,20 @@ const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
       case "green":
         return "bg-[#1bb86c] text-white hover:bg-[#1bb86c]/90";
       case "blue":
-        return "bg-[#1d58df] text-white hover:bg-[#1d58df]/90 disabled:bg-[#14233c] disabled:text-[#476d93] disabled:cursor-not-allowed";
+        return `
+          bg-[radial-gradient(circle_at_center,_#1c52e2,_#001a66)]
+          bg-blend-overlay
+          before:content-[''] before:absolute before:inset-0
+          before:bg-[repeating-radial-gradient(circle,rgba(255,255,255,0.015)_0,rgba(255,255,255,0.015)_1px,transparent_1px,transparent_3px)]
+          before:opacity-30
+          before:pointer-events-none
+          text-white
+          relative
+          hover:brightness-110
+          disabled:opacity-50
+          disabled:hover:brightness-100
+          disabled:cursor-not-allowed
+        `;
       case "secondary":
         return "bg-[#dddfe6] text-black dark:bg-[#4e5973] dark:text-white hover:bg-[#dddfe6]/90 dark:hover:bg-[#4e5973]/90";
       default:
@@ -24,7 +37,7 @@ const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
     <button
       className={twMerge(
         "flex items-center justify-center min-w-[336px] h-[47px] rounded-[200px] py-[16px] px-[24px]",
-        "cursor-pointer font-bold text-xs",
+        "cursor-pointer font-bold text-xs transition-all duration-200",
         getButtonTheme(),
       )}
       {...props}
