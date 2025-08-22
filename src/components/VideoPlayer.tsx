0c333d578ddcbda908d81eaa426741016f18db89
Rodrigo Teixeira <rodrigo.teixeira@intmed.com.br>
2025-08-21 22:50:32 -0300

    chore - video player mixin

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -33,11 +33,11 @@ const VideoPlayer = ({ videoSource, onVideoLoad, onVideoLoadDone, onVideoEnd, on
   // Handle volume change
   const handleVolumeChange = (e: { target: { value: any; }; }) => {
     if (videoRef.current) {
-      console.log(e.target.value)
       const newVolume = e.target.value;
-      setVolume(newVolume);
-      videoRef.current.volume = newVolume;
-      setIsMuted(newVolume === 0);
+      setVolume(+newVolume);
+      videoRef.current.volume = +newVolume;
+
+      setIsMuted(videoRef.current.volume === 0.00);
     }
   };
 
@@ -123,9 +123,9 @@ const VideoPlayer = ({ videoSource, onVideoLoad, onVideoLoadDone, onVideoEnd, on
   }, [isPlaying, volume]);
 
   return (
-    <div className="w-full h-full mx-auto bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+    <div className="flex flex-col justify-end w-full h-full mx-auto bg-gray-800 rounded-lg overflow-hidden shadow-lg">
       {/* Video Container - Fixed aspect ratio */}
-      <div className="relative bg-black" style={{ paddingTop: '62.25%' }}>
+      <div className="relative bg-black pt-[62.25%]">
         <div className="absolute inset-0 flex justify-center items-center ">
           {isLoading && (
             <Loader className="animate-spin" />
@@ -193,11 +193,11 @@ const VideoPlayer = ({ videoSource, onVideoLoad, onVideoLoadDone, onVideoEnd, on
               className="text-white hover:text-blue-400 focus:outline-none"
               aria-label={isMuted ? 'Unmute' : 'Mute'}
             >
-              {isMuted || volume === 0 ? (
+              {isMuted ? (
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
-              ) : volume > 0.5 ? (
+              ) : volume > 0.01 ? (
                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z" clipRule="evenodd" />
                 </svg>
